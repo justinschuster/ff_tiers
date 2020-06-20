762a80b86eb574c51c4dca3022f25971035f6d78
justinschuster <schujustin@gmail.com>
2020-06-20 17:02:50 -0400

    Created file to set the python path so we can import project files.

---
+++ tests/main.py
@@ -0,0 +1,12 @@
+"""
+Written by Justin Schuster
+This file sets python path for the project.
+"""
+
+import os, sys
+
+def set_path():
+    return '/home/justin/ff_tiers'
+
+head_path = set_path()
+sys.path.append(head_path)
