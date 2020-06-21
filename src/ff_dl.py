822cc504fb8c819d89286d1b13a1bac3f818a92a
justinschuster <schujustin@gmail.com>
2020-06-21 14:57:15 -0400

    Uses argparse now

--- src/ff_dl.py
+++ src/ff_dl.py
@@ -5,27 +5,17 @@ the Fantasy Football Pros website.
 Heavily influenced by borischen.co's ff_dl.py file. 
 """
 import sys
-import getopt
+import argparse
 
 def test():
     print("hello")
 
-def main(argv):
-    url = ''
-    try:
-        opts, args = getopt.getopt(argv, "hu:", ["url="])
-    except getopt.GetoptError:
-        print("ff_dl.py -u <url>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-h":
-            print('ff_dl.py -u <url>')
-            sys.exit()
-        elif opt == "-u":
-            url = arg
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", dest="url", help="FantasyPros url", required=True)
+    args = parser.parse_args()
+    url = args.url
     print(url)
 
-
 if __name__ == "__main__":
-    print("Finished")
-    
+    main()
