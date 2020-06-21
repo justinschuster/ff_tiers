d61a39dbe6fd2c26b9803cb6be12fdef3e128aa1
justinschuster <schujustin@gmail.com>
2020-06-21 14:57:32 -0400

    Wrote test case for the ff_dl url retrieval.

--- src/tests/ff_dl_test.py
+++ src/tests/ff_dl_test.py
@@ -3,14 +3,27 @@ Written by Justin Schuster
 This file tests the functions found in
 ff_dl.py.
 """
-import unittest
+# Got this from stackoverflow so I could import ff_dl 
 import os, sys, inspect
-
-# Hacked this up so I could import ff_dl 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 
+import unittest
+import pytest
 import ff_dl 
 
-ff_dl.test()
+class TestClass(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def capfd(self, capfd):
+        self.capfd = capfd
+        
+    def test_url_arg(self):
+        os.system("python3 ~/ff_tiers/src/ff_dl.py -u www.test.com")
+        captured = self.capfd.readouterr()
+        #self.assertEqual('www.test.com\n', captured.out)
+        assert captured.out == "www.test.com\n"
+        
+
+if __name__ == "__main__":
+        unittest.main()
