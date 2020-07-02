8bfc94514513c6e924aafafb4b768b18fbbc03e5
justinschuster <schujustin@gmail.com>
2020-07-02 16:23:33 -0400

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 
 if __name__ == '__main__':
-    os.system("python3 ~/ff_tiers/src/ff_dl.py -u https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php?export=xls -f ~/ff_tiers/data/rankings.html")
+    os.system("python3 ~/ff_tiers/src/ff_dl.py -u https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php?export=xls -f ~/ff_tiers/data/rankings.html")
     os.system("python3 ~/ff_tiers/src/create_plot.py -p QB")
     os.system("python3 ~/ff_tiers/src/create_plot.py -p RB")
     os.system("python3 ~/ff_tiers/src/create_plot.py -p WR")
