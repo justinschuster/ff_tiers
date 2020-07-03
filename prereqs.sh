61dd301d6eb85f8e78a7698fb20ebab2b95753b5
justinschuster <schujustin@gmail.com>
2020-07-03 14:58:37 -0400

    More install stuff

--- prereqs.sh
+++ prereqs.sh
@@ -1,4 +1,8 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
 pip install --upgrade pip
+python3 -m pip install --user virtualenv
+python3 -m venv env 
 pip install requests
 pip3 install beautifulsoup4
 pip install lxml
@@ -6,4 +10,3 @@ pip install -U pytest
 pip install pandas
 pip install -U scikit-learn
 pip install -U matplotlib
-pip install Django 
