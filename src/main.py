f74718c7d512d496e98c5985f25c4b4b33a48e92
justinschuster <schujustin@gmail.com>
2020-07-13 12:43:04 -0400

    Removed exta for loop

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     user_info = {'username':'schujustin', 'password':'justin1', 'token':'1'}
     file_names = ["standard", "ppr", "half-ppr"]
     positions = ['QB', 'RB', 'WR', 'TE', 'K']
-    file_names = ["standard", "ppr", "half-ppr"]
 
     for i in range(0, 3):
         url = "https://www.fantasypros.com/nfl/rankings/{}-cheatsheets.php?export=xls".format(urls[i])
@@ -16,8 +15,7 @@ if __name__ == '__main__':
         player_info = ff_dl.get_player_data(file_name)
         ff_dl.create_csv_file(file_names[i], player_info)  
 
-    for scoring_sys in file_names:
-        data = create_plot.get_data(scoring_sys)
+        data = create_plot.get_data(file_names[i])
         for pos in positions:
             position_data = create_plot.get_position_data(data, pos)
             position_data = position_data[:50]
@@ -25,4 +23,4 @@ if __name__ == '__main__':
             position_data = create_plot.handle_missing_values(position_data)
             position_data['cluster'] = create_plot.clustering(position_data, 7)
             labels = position_data['cluster'].unique()
-            create_plot.plot_cluster(position_data, labels, pos, scoring_sys)
+            create_plot.plot_cluster(position_data, labels, pos, file_names[i])
