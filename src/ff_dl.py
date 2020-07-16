34a2b8056a2da5656e6e1d1a22fafcf040722aa0
justinschuster <schujustin@gmail.com>
2020-07-16 13:28:42 -0400

    ff_dl.py now adheres to pep-8

--- src/ff_dl.py
+++ src/ff_dl.py
@@ -1,39 +1,49 @@
+"""Contains methods that scrape the FantasyPros.com rankings.
+
+Downloads the FantasyPros.com consensus rankings html files then scrapes
+the data using BeautifulSoup4.
+"""
+
 import requests
 
 from bs4 import BeautifulSoup
 
 def get_player_data(file_name):
+    """Scrapes player data from downloaded FP.com ranking html file. """
+
     players = []
     column_headings = [
-            'consensus_ranking', 'player_name', 'team_name_abbrev', 'position', 
-            'bye_week', 'best_ranking', 'worst_ranking', 'average_ranking', 
-            'ranking_std', 'ADP', 'vs_ADP'
+        'consensus_ranking', 'player_name', 'team_name_abbrev', 'position',
+        'bye_week', 'best_ranking', 'worst_ranking', 'average_ranking',
+        'ranking_std', 'ADP', 'vs_ADP'
     ]
 
     players.append(column_headings)
-    with open(file_name) as fp:
-        soup = BeautifulSoup(fp, "lxml")
+    with open(file_name) as rankings_file:
+        soup = BeautifulSoup(rankings_file, "lxml")
     for player in soup.find_all("tr", class_="player-row"):
         player_data = []
         player_td_elements = player.find_all("td")
-        player_data.append(player_td_elements[0].contents[0]) # append index
+        player_data.append(player_td_elements[0].contents[0])
         info = player_td_elements[1].find("input", class_="wsis").attrs
-        player_data.append(info['data-name']) # append player name
-        player_data.append(info['data-team']) # append abbreviated team name  
-        player_data.append(info['data-position']) # append player position
+        player_data.append(info['data-name'])
+        player_data.append(info['data-team'])
+        player_data.append(info['data-position'])
 
         for i in range(4, 11):
             try:
                 player_data.append(player_td_elements[i].contents[0])
-            except:
+            except IndexError:
                 player_data.append('')
 
         players.append(player_data)
     return players
-    
-def download_rankings(url, file_name, user_info): 
+
+def download_rankings(url, file_name, user_info):
+    """Downloads and saves html file."""
+
     resp = requests.get(url,
-                        auth=(user_info['username'], user_info['password'])) 
+                        auth=(user_info['username'], user_info['password']))
     output = open(file_name, 'w+b')
     output.write(resp.content)
     output.close()
