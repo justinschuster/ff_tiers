6ca5f7de37621d3d8d96f0f7024f46a845a08827
justinschuster <schujustin@gmail.com>
2020-07-16 13:18:13 -0400

    removed useless import statements and methods

--- src/ff_dl.py
+++ src/ff_dl.py
@@ -1,23 +1,16 @@
-import sys
-import argparse
 import requests
-import csv
 
 from bs4 import BeautifulSoup
 
-def create_csv_file(scoring_system, player_info):
-    with open('/home/schuj/ff_tiers/data/{}-rankings.csv'.format(scoring_system), 'w+', newline='') as csv_file:
-        wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for player in player_info:
-            wr.writerow(player)
-
 def get_player_data(file_name):
     players = []
-    column_headings = ['consensus_ranking', 'player_name', 'team_name_abbrev', 'position', 'bye_week', 'best_ranking', 'worst_ranking',
-            
-                        'average_ranking', 'ranking_std', 'ADP', 'vs_ADP']
-    players.append(column_headings)
+    column_headings = [
+            'consensus_ranking', 'player_name', 'team_name_abbrev', 'position', 
+            'bye_week', 'best_ranking', 'worst_ranking', 'average_ranking', 
+            'ranking_std', 'ADP', 'vs_ADP'
+    ]
 
+    players.append(column_headings)
     with open(file_name) as fp:
         soup = BeautifulSoup(fp, "lxml")
     for player in soup.find_all("tr", class_="player-row"):
@@ -39,16 +32,8 @@ def get_player_data(file_name):
     return players
     
 def download_rankings(url, file_name, user_info): 
-    resp = requests.get(url, auth=(user_info['username'], user_info['password'])) 
+    resp = requests.get(url,
+                        auth=(user_info['username'], user_info['password'])) 
     output = open(file_name, 'w+b')
     output.write(resp.content)
     output.close()
-
-def get_url():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", dest="url", help="FantasyPros url", required=True)
-    parser.add_argument("-f", dest="file_name", help="Name of rankings file", required=True)
-    args = parser.parse_args()
-    url = args.url
-    file_name = args.file_name
-    return url, file_name
