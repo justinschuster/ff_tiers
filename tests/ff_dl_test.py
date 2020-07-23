8f65237a01a86424aa299be95379ade5fe69776d
justinschuster <schujustin@gmail.com>
2020-07-23 12:42:56 -0400

    Changes urls and linted

--- tests/ff_dl_test.py
+++ tests/ff_dl_test.py
@@ -1,20 +1,34 @@
-import unittest
-import pytest
-import sys
+""" Contains testing methods for ff_dl.py.
 
-sys.path.insert(0,'/home/justin/ff_tiers/src/')
+Tests the functions and uses of ff_dl.py.
+"""
+
+import sys
+import unittest
+
+sys.path.insert(0, '/home/schuj/ff_tiers/src/')
 
 import ff_dl
 
 class TestClass(unittest.TestCase):
+    """ Class for test functions for ff_dl.py """
+
     def test_get_player_data_correct_length(self):
-        file = '/home/justin/ff_tiers/data/rankings.html'
+        """ Checks to see if the player data is correct length. """
+
+        file = '/home/schuj/ff_tiers/data/standard-rankings.html'
         self.assertEqual(496, len(ff_dl.get_player_data(file)))
 
     def test_player_data_column_headings(self):
-        column_headings = ['consensus_ranking', 'player_name', 'team_name_abbrev', 'position', 'bye_week', 'best_ranking', 'worst_ranking', 'average_ranking', 'ranking_std', 'ADP', 'vs ADP'] 
-        file = '/home/justin/ff_tiers/data/rankings.html'
-        self.assertEqual(column_headings, ff_dl.get_player_data(file)[0]) 
+        """ Checks for proper column headings. """
+
+        column_headings = [
+            'consensus_ranking', 'player_name', 'team_name_abbrev',
+            'position', 'bye_week', 'best_ranking', 'worst_ranking',
+            'average_ranking', 'ranking_std', 'ADP', 'vs_ADP'
+        ]
+        file = '/home/schuj/ff_tiers/data/standard-rankings.html'
+        self.assertEqual(column_headings, ff_dl.get_player_data(file)[0])
 
 if __name__ == "__main__":
-        unittest.main()
+    unittest.main()
