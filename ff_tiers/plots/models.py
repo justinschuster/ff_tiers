6db5df66ee82ec16ae8105b231f7cfab212bfede
justinschuster <schujustin@gmail.com>
2020-08-14 12:46:38 -0400

    Created admin and registered plots with admin site

--- ff_tiers/plots/models.py
+++ ff_tiers/plots/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from os import path
+from django.db import models
+from django.utils import timezone
+
+def plots_path():
+    return os.path.join(setting.LOCAL_FILE_DIR, 'plots')
+
+class Plots(models.Model):
+    # Scoring format Constants
+    STANDARD = 'STND'
+    PPR = 'FPPR'
+    HALF_PPR = 'HPPR'
+    SCORING_FORMAT_CHOICES = (
+        (STANDARD, 'standard'),
+        (PPR, 'ppr'),
+        (HALF_PPR, 'half_ppr'),
+    )
+
+    # Position Constants
+    QB = 'QB'
+    RB = 'RB'
+    WR = 'WR'
+    TE = 'TE'
+    K = 'K'
+    POSITION_CHOICES = (
+        (QB, 'QB'),
+        (RB, 'RB'),
+        (WR, 'WR'),
+        (TE, 'TE'),
+        (K, 'K'),
+    )
+
+    plot_path = models.FilePathField(path=plots_path)
+    creation_date = models.DateTimeField('creation_date')
+    scoring_format = models.CharField(
+        max_length=4,
+        choices=SCORING_FORMAT_CHOICES,
+        default=STANDARD,
+    )
+    position = models.CharField(
+        max_length=4,
+        choices=POSITION_CHOICES,
+        default=QB,
+    )
+
+    def __str__(self):
+        return self.plot_path
+
+    def was_created_today(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1)
