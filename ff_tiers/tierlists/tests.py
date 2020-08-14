4b5d18446dda03beb66f2ed97ab59d8e840dd0f4
justinschuster <schujustin@gmail.com>
2020-08-14 18:05:22 -0400

    Created testcase for Plot.was_created_today().

--- ff_tiers/tierlists/tests.py
+++ ff_tiers/tierlists/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Plot
+
+class PlotTestCase(TestCase):
+    def setUp(self):
+        Plot.objects.create(
+            plot_name="test_plot_1",
+            creation_date=timezone.now(),
+            scoring_format='HPPR',
+            position='WR'
+        )
+        Plot.objects.create(
+            plot_name="test_plot_2",
+            creation_date=timezone.now(),
+            scoring_format='FPPR',
+            position='RB'
+        )
+
+    def test_was_created_today(self):
+        """ Plots created today are correctly identified. """
+        test_plot_1 = Plot.objects.get(plot_name='test_plot_1')
+        test_plot_2 = Plot.objects.get(plot_name='test_plot_2')
+        self.assertTrue(Plot.was_created_today(test_plot_1))
+        self.assertTrue(Plot.was_created_today(test_plot_1))
