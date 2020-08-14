83a83fe2d1b1e8fd45a8f468c8e804ce8725c7c1
justinschuster <schujustin@gmail.com>
2020-08-14 15:01:06 -0400

    Plots index page now shows list of plots and links to their pages

--- ff_tiers/plots/views.py
+++ ff_tiers/plots/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Plots
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_plots_list = Plots.objects.order_by('-creation_date')[:5]
+    context = {'latest_plots_list': latest_plots_list}
+    return render(request, 'plots/index.html', context)
 
 def tier_list(request, plots_id):
-    return HttpResponse("ur looking at a plot %s" % plots_id)
+    plot = get_object_or_404(Plots, pk=plots_id)
+    return render(request, 'plots/tier_list.html', {'plot': plot})
