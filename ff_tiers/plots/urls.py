663c7f561d6217fccac0ac532fdc847c243ca44a
justinschuster <schujustin@gmail.com>
2020-08-14 13:33:03 -0400

    Views for individual plots now

--- ff_tiers/plots/urls.py
+++ ff_tiers/plots/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # /plots/2
+    path('<int:plots_id>/', views.tier_list, name='tier_list'),
 ]
--- ff_tiers/plots/views.py
+++ ff_tiers/plots/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World")
 
+def tier_list(request, plots_id):
+    return HttpResponse("ur looking at a plot %s" % plots_id)
