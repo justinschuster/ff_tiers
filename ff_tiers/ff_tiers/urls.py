794480456f06292cc4cea5051001948e2e48fb98
justinschuster <schujustin@gmail.com>
2020-08-14 17:23:50 -0400

    Registered Plot with the admin

--- ff_tiers/ff_tiers/urls.py
+++ ff_tiers/ff_tiers/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('plots/', include('plots.urls')),
+    path('tierlists/', include('tierlists.urls')),
     path('admin/', admin.site.urls),
 ]
