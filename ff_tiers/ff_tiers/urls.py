437f75ba87556bb8213c0c0273cee71e15693612
justinschuster <schujustin@gmail.com>
2020-08-15 16:06:52 -0400

    namespace for tierlists urls

--- ff_tiers/ff_tiers/urls.py
+++ ff_tiers/ff_tiers/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tierlists/', include('tierlists.urls')),
+    path('tierlists/', include('tierlists.urls', namespace='tierlists')),
     path('admin/', admin.site.urls),
 ]
