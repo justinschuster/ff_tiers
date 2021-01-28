3e1ac9cc32957b64e9a556ae38843457a0ac3a59
justinschuster <schujustin@gmail.com>
2021-01-28 18:46:41 -0500

    Choices for scoring and positon

--- backend/django-app/cluster/models.py
+++ backend/django-app/cluster/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Player(models.Model):
+    ScoringType = models.TextChoices('ScoringType', 'STD PPR HPPR')
+    PositionType = models.TextChoices('PositionType', 'QB RB WR TE K')
+    scoring = models.TextField(choices=ScoringType.choices, null=True)
     consensus_ranking = models.IntegerField()
     player_name = models.TextField()
     team_name_abbrev = models.CharField(max_length=10)
-    position = models.CharField(max_length=3)
+    position = models.TextField(choices=PositionType.choices)
     bye_week = models.IntegerField()
     best_ranking = models.IntegerField()
     worst_ranking = models.IntegerField()
