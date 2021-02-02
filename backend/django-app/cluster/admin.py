878b0424c9e028b08c4041ea3f905e9847f9ff30
justinschuster <schujustin@gmail.com>
2021-02-02 10:57:57 -0500

    Player admin view shows more info

--- backend/django-app/cluster/admin.py
+++ backend/django-app/cluster/admin.py
@@ -1,4 +1,22 @@
+"""
+Code for the cluster app Admin page.
+"""
+
 from django.contrib import admin
 from .models import Player
 
-admin.site.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    """
+    Changes admin view for the Player model.
+    """
+    list_display = (
+        'player_name',
+        'consensus_ranking',
+        'position',
+        'best_ranking',
+        'worst_ranking',
+        'average_ranking',
+        'ranking_std'
+    )
+
+admin.site.register(Player, PlayerAdmin)
